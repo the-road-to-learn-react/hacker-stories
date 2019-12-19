2fa51891c930b3952d47d276351c891ccd7a0be8
Robin Wieruch <wrobin@gmx.net>
2019-12-19 13:05:39 +0100

    hs/Reusable-React-Component

--- src/App.js
+++ src/App.js
@@ -49,7 +49,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -58,14 +63,21 @@ const App = () => {
   );
 };
 
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({
+  id,
+  label,
+  value,
+  type = 'text',
+  onInputChange,
+}) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );
