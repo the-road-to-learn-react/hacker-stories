465b53d28558c6c0905dcae5057dc59410b6a74f
Robin Wieruch <wrobin@gmx.net>
2019-12-19 15:55:01 +0100

    hs/Memoized-Handler-in-React

--- src/App.js
+++ src/App.js
@@ -58,8 +58,8 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
-    if (searchTerm === '') return;
+  const handleFetchStories = React.useCallback(() => {
+    if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
@@ -75,6 +75,10 @@ const App = () => {
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = item => {
     dispatchStories({
