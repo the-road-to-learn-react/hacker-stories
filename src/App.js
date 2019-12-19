91e2872bc2e1f8416b7f214e16705cbaf10b85ef
Robin Wieruch <wrobin@gmx.net>
2019-12-19 14:52:50 +0100

    hs/React-Conditional-Rendering

--- src/App.js
+++ src/App.js
@@ -46,11 +46,24 @@ const App = () => {
   );
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
+
     getAsyncStories().then(result => {
       setStories(result.data.stories);
+      setIsLoading(false);
     });
+  }, []);
+
+  React.useEffect(() => {
+    getAsyncStories()
+      .then(result => {
+        setStories(result.data.stories);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
@@ -84,7 +97,16 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
   );
 };
