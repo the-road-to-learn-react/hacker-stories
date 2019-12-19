75a2bc01080661cdd88dcd8fa5f715ccd3b7c559
Robin Wieruch <wrobin@gmx.net>
2019-12-19 12:25:31 +0100

    hs/React-Side-Effects

--- src/App.js
+++ src/App.js
@@ -20,7 +20,13 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState('React');
+  const [searchTerm, setSearchTerm] = React.useState(
+    localStorage.getItem('search') || 'React'
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem('search', searchTerm);
+  }, [searchTerm]);
 
   const handleSearch = event => {
     setSearchTerm(event.target.value);
