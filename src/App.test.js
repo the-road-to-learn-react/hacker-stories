d1b11a2a87d75cf8e7fdb0d06cb368258b623ab8
Robin Wieruch <wrobin@gmx.net>
2020-05-12 16:43:04 +0200

    hs/react-testing-advanced

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
+import { render, fireEvent, act } from '@testing-library/react';
 import axios from 'axios';
 
-import App, { Item, List, SearchForm, InputWithLabel } from './App';
+import App, { Item, List, SearchForm } from './App';
 
 jest.mock('axios');
 
@@ -17,37 +17,25 @@ describe('Item', () => {
   };
   const handleRemoveItem = jest.fn();
 
-  let component;
-
-  beforeEach(() => {
-    component = renderer.create(
-      <Item item={item} onRemoveItem={handleRemoveItem} />
-    );
-  });
-
   it('renders all properties', () => {
-    expect(component.root.findByType('a').props.href).toEqual(
+    const { getByText } = render(<Item item={item} />);
+
+    expect(getByText('Jordan Walke')).toBeTruthy();
+    expect(getByText('React')).toHaveAttribute(
+      'href',
       'https://reactjs.org/'
     );
-
-    expect(
-      component.root.findAllByProps({ children: 'Jordan Walke' })
-        .length
-    ).toEqual(1);
   });
 
   it('calls onRemoveItem on button click', () => {
-    component.root.findByType('button').props.onClick();
+    const { getByText } = render(
+      <Item item={item} onRemoveItem={handleRemoveItem} />
+    );
+
+    fireEvent.click(getByText('Dismiss'));
 
     expect(handleRemoveItem).toHaveBeenCalledTimes(1);
     expect(handleRemoveItem).toHaveBeenCalledWith(item);
-
-    expect(component.root.findAllByType(Item).length).toEqual(1);
-  });
-
-  test('renders snapshot', () => {
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
   });
 });
 
@@ -72,9 +60,13 @@ describe('List', () => {
   ];
 
   it('renders two items', () => {
-    const component = renderer.create(<List list={list} />);
+    const { getAllByText, getAllByRole, container } = render(
+      <List list={list} />
+    );
 
-    expect(component.root.findAllByType(Item).length).toEqual(2);
+    expect(getAllByText('Dismiss').length).toBe(2);
+    expect(getAllByRole('button').length).toBe(2);
+    expect(container.querySelectorAll('button').length).toBe(2);
   });
 });
 
@@ -85,72 +77,70 @@ describe('SearchForm', () => {
     onSearchSubmit: jest.fn(),
   };
 
-  let component;
-
-  beforeEach(() => {
-    component = renderer.create(<SearchForm {...searchFormProps} />);
-  });
-
   it('renders the input field with its value', () => {
-    const value = component.root.findByType('input').props.value;
+    const { getByDisplayValue } = render(
+      <SearchForm {...searchFormProps} />
+    );
 
-    expect(value).toEqual('React');
+    expect(getByDisplayValue('React')).toBeTruthy();
   });
 
-  it('changes the input field', () => {
-    const pseudoEvent = { target: 'Redux' };
+  it('renders the correct label', () => {
+    const { getByLabelText } = render(
+      <SearchForm {...searchFormProps} />
+    );
 
-    component.root.findByType('input').props.onChange(pseudoEvent);
+    expect(getByLabelText('Search:')).toBeTruthy();
+  });
+
+  it('renders a submit button', () => {
+    const { getByText } = render(<SearchForm {...searchFormProps} />);
+
+    expect(getByText('Submit')).toBeTruthy();
+  });
+
+  it('calls onSearchInput on input field change', () => {
+    const { getByDisplayValue } = render(
+      <SearchForm {...searchFormProps} />
+    );
+
+    fireEvent.change(getByDisplayValue('React'), {
+      target: { value: 'Redux' },
+    });
 
     expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
-    expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(
-      pseudoEvent
-    );
   });
 
-  it('submits the form', () => {
-    const pseudoEvent = {};
+  it('calls onSearchSubmit on button submit click', () => {
+    const { getByText } = render(<SearchForm {...searchFormProps} />);
 
-    component.root.findByType('form').props.onSubmit(pseudoEvent);
+    fireEvent.submit(getByText('Submit'));
 
     expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
-    expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(
-      pseudoEvent
-    );
-  });
-
-  it('disables the button and prevents submit', () => {
-    component.update(
-      <SearchForm {...searchFormProps} searchTerm="" />
-    );
-
-    expect(
-      component.root.findByType('button').props.disabled
-    ).toBeTruthy();
   });
 });
 
 describe('App', () => {
-  it('succeeds fetching data with a list', async () => {
-    const list = [
-      {
-        title: 'React',
-        url: 'https://reactjs.org/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-      },
-      {
-        title: 'Redux',
-        url: 'https://redux.js.org/',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-      },
-    ];
+  const list = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
+  it('succeeds fetching data with a list', async () => {
     const promise = Promise.resolve({
       data: {
         hits: list,
@@ -159,13 +149,17 @@ describe('App', () => {
 
     axios.get.mockImplementationOnce(() => promise);
 
-    let component;
+    const { queryByText, queryAllByText } = render(<App />);
 
-    await renderer.act(async () => {
-      component = renderer.create(<App />);
-    });
+    expect(queryByText(/Loading/)).toBeTruthy();
 
-    expect(component.root.findByType(List).props.list).toEqual(list);
+    await act(() => promise);
+
+    expect(queryByText(/Loading/)).toBeFalsy();
+
+    expect(queryByText('Jordan Walke')).toBeTruthy();
+    expect(queryByText('Dan Abramov, Andrew Clark')).toBeTruthy();
+    expect(queryAllByText('Dismiss').length).toBe(2);
   });
 
   it('fails fetching data with a list', async () => {
@@ -173,26 +167,94 @@ describe('App', () => {
 
     axios.get.mockImplementationOnce(() => promise);
 
-    let component;
+    const { queryByText } = render(<App />);
 
-    await renderer.act(async () => {
-      component = renderer.create(<App />);
+    expect(queryByText(/Loading/)).toBeTruthy();
+
+    try {
+      await act(() => promise);
+    } catch (error) {
+      expect(queryByText(/Loading/)).toBeFalsy();
+      expect(queryByText(/Something went wrong/)).toBeTruthy();
+    }
+  });
+
+  it('removes a story from the list', async () => {
+    const promise = Promise.resolve({
+      data: {
+        hits: list,
+      },
     });
 
-    expect(component.root.findByType('p').props.children).toEqual(
-      'Something went wrong ...'
-    );
-  });
-});
+    axios.get.mockImplementationOnce(() => promise);
 
-describe('InputWithLabel', () => {
-  test('renders snapshot', () => {
-    const component = renderer.create(
-      <InputWithLabel id="1" value="a">
-        Input with Label
-      </InputWithLabel>
-    );
-    let tree = component.toJSON();
-    expect(tree).toMatchSnapshot();
+    const { queryByText, getAllByText } = render(<App />);
+
+    await act(() => promise);
+
+    expect(queryByText('Jordan Walke')).toBeTruthy();
+    fireEvent.click(getAllByText('Dismiss')[0]);
+    expect(queryByText('Jordan Walke')).toBeFalsy();
+  });
+
+  it('searches for specific items in the list', async () => {
+    const reactPromise = Promise.resolve({
+      data: {
+        hits: list,
+      },
+    });
+
+    const javascriptPromise = Promise.resolve({
+      data: {
+        hits: [
+          {
+            title: 'JavaScript',
+            url: 'https://en.wikipedia.org/wiki/JavaScript',
+            author: 'Brendan Eich',
+            num_comments: 15,
+            points: 10,
+            objectID: 3,
+          },
+        ],
+      },
+    });
+
+    axios.get.mockImplementation((url) => {
+      if (url.includes('React')) {
+        return reactPromise;
+      }
+
+      if (url.includes('JavaScript')) {
+        return javascriptPromise;
+      }
+
+      throw Error();
+    });
+
+    const { queryByText, queryByDisplayValue } = render(<App />);
+
+    await act(() => reactPromise);
+
+    expect(queryByDisplayValue('React')).toBeTruthy();
+    expect(queryByDisplayValue('JavaScript')).toBeFalsy();
+
+    expect(queryByText('Jordan Walke')).toBeTruthy();
+    expect(queryByText('Dan Abramov, Andrew Clark')).toBeTruthy();
+    expect(queryByText('Brendan Eich')).toBeFalsy();
+
+    fireEvent.change(queryByDisplayValue('React'), {
+      target: { value: 'JavaScript' },
+    });
+
+    expect(queryByDisplayValue('React')).toBeFalsy();
+    expect(queryByDisplayValue('JavaScript')).toBeTruthy();
+
+    fireEvent.submit(queryByText('Submit'));
+
+    await act(() => javascriptPromise);
+
+    expect(queryByText('Jordan Walke')).toBeFalsy();
+    expect(queryByText('Dan Abramov, Andrew Clark')).toBeFalsy();
+    expect(queryByText('Brendan Eich')).toBeTruthy();
   });
 });
