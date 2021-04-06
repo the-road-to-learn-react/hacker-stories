bf4e86ad3fc711d8d8cc59ab970b55f70f214b3f
Robin Wieruch <hello@rwieruch.com>
2021-04-06 15:58:30 +0200

    2021

--- src/App.js
+++ src/App.js
@@ -49,7 +49,13 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -58,14 +64,21 @@ const App = () => {
   );
 };
 
-const Search = ({ search, onSearch }) => (
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  children,
+}) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{children}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );
