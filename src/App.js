c407ae7bf607f74aca99d70070ec20a3beec9297
Robin Wieruch <hello@rwieruch.com>
2021-04-07 19:44:55 +0200

    2021

--- src/App.js
+++ src/App.js
@@ -2,38 +2,7 @@ import * as React from 'react';
 import axios from 'axios';
 import { sortBy } from 'lodash';
 
-const API_BASE = 'https://hn.algolia.com/api/v1';
-const API_SEARCH = '/search';
-const PARAM_SEARCH = 'query=';
-const PARAM_PAGE = 'page=';
-
-const getUrl = (searchTerm, page) =>
-  `${API_BASE}${API_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`;
-
-const extractSearchTerm = (url) =>
-  url
-    .substring(url.lastIndexOf('?') + 1, url.lastIndexOf('&'))
-    .replace(PARAM_SEARCH, '');
-
-const getLastSearches = (urls) =>
-  urls
-    .reduce((result, url, index) => {
-      const searchTerm = extractSearchTerm(url);
-
-      if (index === 0) {
-        return result.concat(searchTerm);
-      }
-
-      const previousSearchTerm = result[result.length - 1];
-
-      if (searchTerm === previousSearchTerm) {
-        return result;
-      } else {
-        return result.concat(searchTerm);
-      }
-    }, [])
-    .slice(-6)
-    .slice(0, -1);
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
   const [value, setValue] = React.useState(
@@ -60,11 +29,7 @@ const storiesReducer = (state, action) => {
         ...state,
         isLoading: false,
         isError: false,
-        data:
-          action.payload.page === 0
-            ? action.payload.list
-            : state.data.concat(action.payload.list),
-        page: action.payload.page,
+        data: action.payload,
       };
     case 'STORIES_FETCH_FAILURE':
       return {
@@ -90,31 +55,29 @@ const App = () => {
     'React'
   );
 
-  const [urls, setUrls] = React.useState([getUrl(searchTerm, 0)]);
+  const [url, setUrl] = React.useState(
+    `${API_ENDPOINT}${searchTerm}`
+  );
 
   const [stories, dispatchStories] = React.useReducer(
     storiesReducer,
-    { data: [], page: 0, isLoading: false, isError: false }
+    { data: [], isLoading: false, isError: false }
   );
 
   const handleFetchStories = React.useCallback(async () => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
     try {
-      const lastUrl = urls[urls.length - 1];
-      const result = await axios.get(lastUrl);
+      const result = await axios.get(url);
 
       dispatchStories({
         type: 'STORIES_FETCH_SUCCESS',
-        payload: {
-          list: result.data.hits,
-          page: result.data.page,
-        },
+        payload: result.data.hits,
       });
     } catch {
       dispatchStories({ type: 'STORIES_FETCH_FAILURE' });
     }
-  }, [urls]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -132,29 +95,10 @@ const App = () => {
   };
 
   const handleSearchSubmit = (event) => {
-    handleSearch(searchTerm, 0);
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
 
     event.preventDefault();
   };
-
-  const handleLastSearch = (searchTerm) => {
-    setSearchTerm(searchTerm);
-
-    handleSearch(searchTerm, 0);
-  };
-
-  const handleMore = () => {
-    const lastUrl = urls[urls.length - 1];
-    const searchTerm = extractSearchTerm(lastUrl);
-    handleSearch(searchTerm, stories.page + 1);
-  };
-
-  const handleSearch = (searchTerm, page) => {
-    const url = getUrl(searchTerm, page);
-    setUrls(urls.concat(url));
-  };
-
-  const lastSearches = getLastSearches(urls);
 
   return (
     <div>
@@ -166,41 +110,18 @@ const App = () => {
         onSearchSubmit={handleSearchSubmit}
       />
 
-      <LastSearches
-        lastSearches={lastSearches}
-        onLastSearch={handleLastSearch}
-      />
-
       <hr />
 
       {stories.isError && <p>Something went wrong ...</p>}
 
-      <List list={stories.data} onRemoveItem={handleRemoveStory} />
-
       {stories.isLoading ? (
         <p>Loading ...</p>
       ) : (
-        <button type="button" onClick={handleMore}>
-          More
-        </button>
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </div>
   );
 };
-
-const LastSearches = ({ lastSearches, onLastSearch }) => (
-  <>
-    {lastSearches.map((searchTerm, index) => (
-      <button
-        key={searchTerm + index}
-        type="button"
-        onClick={() => onLastSearch(searchTerm)}
-      >
-        {searchTerm}
-      </button>
-    ))}
-  </>
-);
 
 const SearchForm = ({
   searchTerm,
@@ -263,21 +184,14 @@ const SORTS = {
 };
 
 const List = ({ list, onRemoveItem }) => {
-  const [sort, setSort] = React.useState({
-    sortKey: 'NONE',
-    isReverse: false,
-  });
+  const [sort, setSort] = React.useState('NONE');
 
   const handleSort = (sortKey) => {
-    const isReverse = sort.sortKey === sortKey && !sort.isReverse;
-
-    setSort({ sortKey, isReverse });
+    setSort(sortKey);
   };
 
-  const sortFunction = SORTS[sort.sortKey];
-  const sortedList = sort.isReverse
-    ? sortFunction(list).reverse()
-    : sortFunction(list);
+  const sortFunction = SORTS[sort];
+  const sortedList = sortFunction(list);
 
   return (
     <ul>
