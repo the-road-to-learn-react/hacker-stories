ab2355df2f6ed5cffebbd49a0c33dd7d63a3f761
Robin Wieruch <hello@rwieruch.com>
2021-04-07 15:38:51 +0200

    2021

---
+++ src/InputWithLabel.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}) => {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input
+        id={id}
+        ref={inputRef}
+        type={type}
+        value={value}
+        onChange={onInputChange}
+      />
+    </>
+  );
+};
+
+export { InputWithLabel };
