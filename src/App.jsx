da47deeb53404781b2fec85c14e78140b429e941
Robin Wieruch <hello@rwieruch.com>
2022-09-21 16:04:16 +0200

    2022/React-State

--- src/App.jsx
+++ src/App.jsx
@@ -34,17 +34,20 @@ const App = () => {
 };
 
 const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
   const handleChange = (event) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
+    setSearchTerm(event.target.value);
   };
 
   return (
     <div>
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
     </div>
   );
 };
