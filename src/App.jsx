c0afa0c88a4edf556637d957719f8c39fa06a99d
Robin Wieruch <hello@rwieruch.com>
2022-09-21 15:43:04 +0200

    2022/React-JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
 
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
     </div>
   );
 }
