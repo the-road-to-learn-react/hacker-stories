d77aad74d63e4a4e9d7fa2690a65debbf8fbf2a0
Robin Wieruch <hello@rwieruch.com>
2022-09-27 14:41:02 +0200

    2022/Memoized-Handler-in-React-TS

--- src/App.tsx
+++ src/App.tsx
@@ -105,7 +105,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
@@ -122,6 +122,10 @@ const App = () => {
         dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
       );
   }, [searchTerm]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item: Story) => {
     dispatchStories({
