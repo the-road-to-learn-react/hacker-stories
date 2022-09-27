388dad85189bd9bb4d4a9b8797d8be4ef27dc1b8
Robin Wieruch <hello@rwieruch.com>
2022-09-27 13:44:54 +0200

    2022/Props-Handling-TS

--- src/App.tsx
+++ src/App.tsx
@@ -61,14 +61,14 @@ type SearchProps = {
   onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Search: React.FC<SearchProps> = (props) => (
+const Search: React.FC<SearchProps> = ({ search, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
     <input
       id="search"
       type="text"
-      value={props.search}
-      onChange={props.onSearch}
+      value={search}
+      onChange={onSearch}
     />
   </div>
 );
@@ -77,9 +77,9 @@ type ListProps = {
   list: Stories;
 };
 
-const List: React.FC<ListProps> = (props) => (
+const List: React.FC<ListProps> = ({ list }) => (
   <ul>
-    {props.list.map((item) => (
+    {list.map((item) => (
       <Item key={item.objectID} item={item} />
     ))}
   </ul>
@@ -89,14 +89,14 @@ type ItemProps = {
   item: Story;
 };
 
-const Item: React.FC<ItemProps> = (props) => (
+const Item: React.FC<ItemProps> = ({ item }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={item.url}>{item.title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{item.author}</span>
+    <span>{item.num_comments}</span>
+    <span>{item.points}</span>
   </li>
 );
 
