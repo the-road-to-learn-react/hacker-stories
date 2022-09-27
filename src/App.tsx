df5d1b85b78f9fe70047686ae801976cb6ca05e9
Robin Wieruch <hello@rwieruch.com>
2022-09-27 13:54:20 +0200

    2022/Reusable-React-Component-TS

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,12 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search search={searchTerm} onSearch={handleSearch} />
+      <InputWithLabel
+        id="search"
+        label="Search"
+        value={searchTerm}
+        onInputChange={handleSearch}
+      />
 
       <hr />
 
@@ -74,19 +79,29 @@ const App = () => {
   );
 };
 
-type SearchProps = {
-  search: string;
-  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
+type InputWithLabelProps = {
+  id: string;
+  label: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Search: React.FC<SearchProps> = ({ search, onSearch }) => (
+const InputWithLabel: React.FC<InputWithLabelProps> = ({
+  id,
+  label,
+  value,
+  type = 'text',
+  onInputChange,
+}) => (
   <>
-    <label htmlFor="search">Search: </label>
+    <label htmlFor={id}>{label}</label>
+    &nbsp;
     <input
-      id="search"
-      type="text"
-      value={search}
-      onChange={onSearch}
+      id={id}
+      type={type}
+      value={value}
+      onChange={onInputChange}
     />
   </>
 );
