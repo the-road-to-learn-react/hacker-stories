23ad50e50f87949f7b8609940698b736e4626b49
Robin Wieruch <hello@rwieruch.com>
2022-09-27 14:56:03 +0200

    2022/Third-Party-Libraries-in-React-TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import axios from 'axios';
 
 type Story = {
   objectID: string;
@@ -112,12 +113,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() =>
