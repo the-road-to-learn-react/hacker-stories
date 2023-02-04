b99f45ac3ff00331f3f331f989f0a03387bf3987
Robin Wieruch <hello@rwieruch.com>
2023-02-04 16:33:28 +0100

    2022/linting-with-eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import * as React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+const title = 'React';
 
 function App() {
   return (
     <div>
-      <h1>Hello {getTitle('React')}</h1>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <h1>Hello World</h1>
     </div>
   );
 }
