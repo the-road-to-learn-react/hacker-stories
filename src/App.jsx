9e7be781ef3e823027cc8ce806afe5e4c89e0edd
Robin Wieruch <hello@rwieruch.com>
2024-01-18 15:14:13 +0100

    feat(2024-component-composition): 2024-component-composition

--- src/App.jsx
+++ src/App.jsx
@@ -51,10 +51,11 @@ const App = () => {
 
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-      />
+      >
+        <strong>Search:</strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -65,13 +66,13 @@ const App = () => {
 
 const InputWithLabel = ({
   id,
-  label,
   value,
   type = 'text',
   onInputChange,
+  children,
 }) => (
   <>
-    <label htmlFor={id}>{label}</label>
+    <label htmlFor={id}>{children}</label>
     &nbsp;
     <input
       id={id}
