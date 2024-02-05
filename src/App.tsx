7e95573e73c855533994f8aa2c93e6948df3e5eb
Robin Wieruch <hello@rwieruch.com>
2024-02-05 11:10:44 +0100

    feat(2024-controlled-components-ts): 2024-controlled-components-ts

--- src/App.tsx
+++ src/App.tsx
@@ -29,36 +29,47 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('React');
+
+  const handleSearch = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const searchedStories = stories.filter((story) =>
+    story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
-      <List list={stories} />
+      <List list={searchedStories} />
     </div>
   );
 };
 
-const Search = () => {
-  const handleChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    // synthetic event
-    console.log(event);
-    // value of target (here: input HTML element)
-    console.log(event.target.value);
-  };
-
-  return (
-    <div>
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange} />
-    </div>
-  );
+type SearchProps = {
+  search: string;
+  onSearch: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
+
+const Search = (props: SearchProps) => (
+  <div>
+    <label htmlFor="search">Search: </label>
+    <input
+      id="search"
+      type="text"
+      value={props.search}
+      onChange={props.onSearch}
+    />
+  </div>
+);
 
 type ListProps = {
   list: Story[];
