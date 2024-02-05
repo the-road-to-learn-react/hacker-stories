f825af6eae0e46b1fe3a1f338a0171be97d67915
Robin Wieruch <hello@rwieruch.com>
2024-02-05 10:57:30 +0100

    feat(2024-props-ts): 2024-props-ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
 import * as React from 'react';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+type Story = {
+  objectID: number;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
+};
 
-const App = () => (
-  <div>
-    <h1>My Hacker Stories</h1>
+type Stories = Story[];
 
-    <Search />
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-    <hr />
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
 
-    <List />
-  </div>
-);
+      <Search />
+
+      <hr />
+
+      <List list={stories} />
+    </div>
+  );
+};
 
 const Search = () => {
   const handleChange = (
@@ -49,19 +62,31 @@ const Search = () => {
   );
 };
 
-const List = () => (
+type ListProps = {
+  list: Stories;
+};
+
+const List: React.FC<ListProps> = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-      </li>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
   </ul>
+);
+
+type ItemProps = {
+  item: Story;
+};
+
+const Item: React.FC<ItemProps> = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
 );
 
 export default App;
