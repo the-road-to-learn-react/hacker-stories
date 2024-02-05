48cd498f737d1d5225120cafcc41c6b0799607f6
Robin Wieruch <hello@rwieruch.com>
2024-02-05 11:43:09 +0100

    fix(2024-conditiona-rendering-ts): 2024-conditiona-rendering-ts

--- src/App.tsx
+++ src/App.tsx
@@ -55,11 +55,18 @@ const App = () => {
   );
 
   const [stories, setStories] = React.useState<Story[]>([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
-    getAsyncStories().then((result) => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then((result) => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = (item: Story) => {
@@ -95,7 +102,16 @@ const App = () => {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong ...</p>}
+
+      {isLoading ? (
+        <p>Loading ...</p>
+      ) : (
+        <List
+          list={searchedStories}
+          onRemoveItem={handleRemoveStory}
+        />
+      )}
     </div>
   );
 };
