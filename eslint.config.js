af941f205d6dcffaf271e93e1bcafc911f2e3446
Robin Wieruch <hello@rwieruch.com>
2025-01-30 14:12:29 +0100

    fix(2025_performance): 2025_performance

--- eslint.config.js
+++ eslint.config.js
@@ -29,6 +29,7 @@ export default [
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
       'react/prop-types': 'off',
+      'react/display-name': 'off',
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': [
         'warn',
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,5 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')).render(<App />);
