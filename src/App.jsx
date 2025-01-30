a5f41e0a56cf46b152a9119d14245e62a7a5541d
Robin Wieruch <hello@rwieruch.com>
2025-01-30 11:02:06 +0100

    fix(2025_handler-function): 2025_handler-function

--- src/App.jsx
+++ src/App.jsx
@@ -29,12 +29,21 @@ const App = () => (
   </div>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
-);
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+    </div>
+  );
+};
 
 const List = () => (
   <ul>
