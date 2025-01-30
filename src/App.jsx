4845b011a85833fe4b34b43819a54fc2d4a11dd1
Robin Wieruch <hello@rwieruch.com>
2025-01-30 11:09:51 +0100

    fix(2025_callback-handlers): 2025_callback-handlers

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,15 @@ const App = () => {
     },
   ];
 
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+  };
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
 
@@ -33,11 +37,13 @@ const App = () => {
   );
 };
 
-const Search = () => {
+const Search = (props) => {
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+
+    props.onSearch(event);
   };
 
   return (
